8662a4e775d20024e88148d7f50b0b5126ebe8e8
Ashish Poonawalla <ashish.omar.pune@gmail.com>
2024-01-22 22:47:59 +0530

    Nav Finished

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,43 +7,52 @@ import { navItems } from "./navItems";
 const Navbar = () => {
     const [isMenuOpen, setIsMenuOpen] = useState(false)
 
-    const toggleMenu =()=>{
+    const toggleMenu = () => {
         setIsMenuOpen(!isMenuOpen)
     }
 
     return (
-        <nav className="bg-white md:px-14 p-4 max-w-screen-2xl mx-auto text-primary">
-            <div className="text-lg container mx-auto flex justify-between items-center font-medium">
-                <div className="flex space-x-14 items-center">
-                    <a href="/" className="text-2xl font-semibold flex items-center space-x-3 text-primary">
-                        <img src={logo} alt="" className="w-10 inline-block items-center" /><span>XYZ</span>
-                    </a>
+        <>
+            <nav className="bg-white md:px-14 p-4 max-w-screen-2xl mx-auto text-primary fixed top-0 right-0 left-0">
+                <div className="text-lg container mx-auto flex justify-between items-center font-medium">
+                    <div className="flex space-x-14 items-center">
+                        <a href="/" className="text-2xl font-semibold flex items-center space-x-3 text-primary">
+                            <img src={logo} alt="" className="w-10 inline-block items-center" /><span>XYZ</span>
+                        </a>
 
-                    {/* nav items */}
-                    <ul className="md:flex space-x-12 hidden">
-                        {
-                            navItems.map(({ link, path }) => <a key={link} href={path} className="block hover:text-gray-300">{link}</a>)
-                        }
-                    </ul>
-                </div>
+                        {/* nav items */}
+                        <ul className="md:flex space-x-12 hidden">
+                            {
+                                navItems.map(({ link, path }) => <a key={link} href={path} className="block hover:text-gray-300">{link}</a>)
+                            }
+                        </ul>
+                    </div>
 
-                {/* language & signup */}
-                <div className="space-x-12 hidden md:flex items-center">
-                    <a href="/" className="hidden lg:flex items-center hover:text-secondary"><GrLanguage className="mr-2" /><span>Language</span></a>
-                    <button className="bg-secondary py-2 px-4 transition-all duration-300 rounded
+                    {/* language & signup */}
+                    <div className="space-x-12 hidden md:flex items-center">
+                        <a href="/" className="hidden lg:flex items-center hover:text-secondary"><GrLanguage className="mr-2" /><span>Language</span></a>
+                        <button className="bg-secondary py-2 px-4 transition-all duration-300 rounded
                          hover:text-white hover:bg-indigo-600">Sign up</button>
-                </div>
+                    </div>
 
-                {/* menu btn. only display on mobile */}
-                <div className="md:hidden">
-                    <button onClick={toggleMenu} className="text-white focus:outline-none focus:text-gray-300">
-                    {
-                        isMenuOpen ? (<FaXmark/>): (<FaBars/>)
-                    }
-                    </button>
+                    {/* menu btn. only display on mobile */}
+                    <div className="md:hidden">
+                        <button onClick={toggleMenu} className="text-white text-xl focus:outline-none
+                     focus:text-gray-300">
+                            {
+                                isMenuOpen ? (<FaXmark className="w-6 h-6 text-primary " />) : (<FaBars className="w-6 h-6 text-primary" />)
+                            }
+                        </button>
+                    </div>
                 </div>
+            </nav>
+
+            <div className={`space-y-4 px-4 pt-24 pb-5 bg-secondary ${isMenuOpen ? "block fixed top-0 right-0 left-0" : "hidden" }`}>
+                {
+                    navItems.map(({ link, path }) => <a key={link} href={path} className="block hover:text-gray-300">{link}</a>)
+                }
             </div>
-        </nav>
+        </>
     )
 }
 
