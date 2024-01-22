022a39802839184cdc1d891b984f3cfc97b44569
Ashish Poonawalla <ashish.omar.pune@gmail.com>
2024-01-22 23:01:12 +0530

    update

---
+++ src/components/Home.jsx
@@ -0,0 +1,16 @@
+const Home = () => {
+    return (
+        <div className="md:px-12 p-4 max-w-screen-2xl mx-auto mt-28">
+            <div>
+                <div>
+                    {/* banner content */}
+                    <div>
+                        <h2>Develop your skills without diligence</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home
