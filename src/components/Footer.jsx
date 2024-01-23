bf24d084aa839035b200a2e46db0f29fad8a81fb
Ashish Poonawalla <ashish.omar.pune@gmail.com>
2024-01-23 12:34:45 +0530

    update

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import logo from "../assets/logo.png";
+
+const Footer = () => {
+    return (
+        <div>
+            <div>
+                <div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
